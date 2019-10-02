a3fd68c1e20dff39fd84ab6c2f1ee9dd8bc9386f
Eric Sng <ericsng@Erics-Mac-mini.home.lan>
2019-10-02 21:00:00 +0800

    Added unicode conversion code

---
+++ scripts/unicodetranspose.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+
+  // Converts the first char of var inputText to unicode based
+  // on the format in var format
+  function transposeCharToUnicode(inputText, format) {
+    var offset=0;
+
+    if (format == "I")
+    {
+      // offset for italic unicode
+
+      // italic serif
+      // offset = 0x1D434 - 0x47;
+
+      // italic sans serif
+      offset = 0x1D608 - 0x47;
+
+    }
+    else if (format == "B")
+    {
+      // offset for bold unicode
+
+      // bold serif
+      // offset = 0x1D400- 0x47;
+
+      //bold sans serif
+      offset = 0x1D5D4- 0x47;
+    }
+    else if (format == "X")
+    {
+      // offset for bold italic unicode
+
+      // bold italic serif
+      // offset = 0x1D468- 0x47;
+
+      // bold italic sans serif
+      offset = 0x1D63C- 0x47;
+    }
+
+    // return appropriate uncode string
+    if (inputText.charAt(0) >= "A" && inputText.charAt(0) <= "Z")
+      return (inputText.charCodeAt(0)+offset + 6).toString(16);
+    else if (inputText.charAt(0) >= "a" && inputText.charAt(0) <= "z")
+      return (inputText.charCodeAt(0)+offset).toString(16);
+    else return inputText.charAt(0); // fallthrough: return inputText
+
+  }
+
+  // Converts the string in var inputText to unicode string based
+  // on the format in var format
+  function transposeStringToUnicode(inputText, format) {
+    var convertedText ="";
+    var transposedChar;
+
+    for (var i = 0; i < inputText.length; i++) {
+      transposedChar = transposeCharToUnicode(inputText.charAt(i),format)
+      if (transposedChar.length > 1)
+        convertedText += "&#x"+transposedChar+";"; // successful transpose to unicode
+      else
+        convertedText += transposedChar; // failed transpose
+    }
+    return convertedText;
+  }
+
+  function transpose(){
+    var convertedText;
+
+    // clear previous outputText
+    $("#italicText").text("");
+    $("#boldText").text("");
+    $("#boldItalicText").text("");
+
+    // Transpose italic
+    $("#italicText").append(transposeStringToUnicode($("#inputText").val(),"I"));
+    // Transpose bold
+    $("#boldText").append(transposeStringToUnicode($("#inputText").val(),"B"));
+    // Transpose bold italic
+    $("#boldItalicText").append(transposeStringToUnicode($("#inputText").val(),"X"));
+
+    if ($("#inputText").val().length == 0) {
+      $("#italicText").html("&nbsp;");
+      $("#boldText").html("&nbsp;");
+      $("#boldItalicText").html("&nbsp;");
+    }
+    // grow input textarea when needed
+    $("#inputText").height($("#boldItalicText").height());
+  }
+
+  // Transpose event handlers
+  // $("#transposeButton").click(transpose);
+   $("#inputText").keyup(transpose);
+   // $("#inputText").height($("#boldItalicText").height());
+
+   // input textarea gets focus
+   $("#inputText").focus();
+});
