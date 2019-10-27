4545cc6e47768f0e8b825e9642a1ea6842a8ac2c
Eric Sng <ericsng@Erics-Mac-mini.home.lan>
2019-10-27 21:37:14 +0800

    Implement unicode translation using translator classes

--- scripts/unicodetranspose.js
+++ scripts/unicodetranspose.js
@@ -1,104 +1,123 @@
 $(document).ready(function() {
 
-  // Converts the first char of var inputText to unicode based
-  // on the format in var format
-  function transposeCharToUnicode(inputText, format) {
-    var offset=0;
+  // array to hold all translators
+  var translatorList = [];
 
-    if (format == "I")
-    {
-      // offset for italic unicode
+  // class to implement translators
+  class asciiToUnicodeTranslator {
 
-      // italic serif
-      // offset = 0x1D434 - 0x47;
+    displayName = "";
+    outputTextID = "";
+    offset = 0;
+    dictionary = {};
 
-      // italic sans serif
-      offset = 0x1D608 - 0x47;
-
-    }
-    else if (format == "B")
-    {
-      // offset for bold unicode
-
-      // bold serif
-      // offset = 0x1D400- 0x47;
-
-      //bold sans serif
-      offset = 0x1D5D4- 0x47;
-    }
-    else if (format == "X")
-    {
-      // offset for bold italic unicode
-
-      // bold italic serif
-      // offset = 0x1D468- 0x47;
-
-      // bold italic sans serif
-      offset = 0x1D63C- 0x47;
-    }
-    else if (format == "C")
-    {
-      // offset for cursive bold unicode
-
-      // bold cursive
-      offset = 0x1D4D0- 0x47;
+    constructor(unicodeInitialization) {
+      if (unicodeInitialization.offset == 0) {
+        dictionary = unicodeInitialization.dictionary;
+      } else {
+        this.offset = unicodeInitialization.offset;
+      }
+      this.displayName = unicodeInitialization.displayName;
+      this.outputTextID = unicodeInitialization.outputTextID;
     }
 
-    // return appropriate uncode string
-    if (inputText.charAt(0) >= "A" && inputText.charAt(0) <= "Z")
-      return (inputText.charCodeAt(0)+offset + 6).toString(16);
-    else if (inputText.charAt(0) >= "a" && inputText.charAt(0) <= "z")
-      return (inputText.charCodeAt(0)+offset).toString(16);
-    else return inputText.charAt(0); // fallthrough: return inputText
+    writeHTML() {
+      return this.displayName;
+    }
 
+    translateCharToUnicode(inputChar) {
+      if (this.offset > 0) {
+        // use offset
+        if (inputChar.charAt(0) >= "A" && inputChar.charAt(0) <= "Z")
+          return (inputChar.charCodeAt(0)+this.offset + 6).toString(16);
+        else if (inputChar.charAt(0) >= "a" && inputChar.charAt(0) <= "z")
+          return (inputChar.charCodeAt(0)+this.offset).toString(16);
+        else return inputChar.charAt(0); // fallthrough: return inputText
+      } else {
+        // use dictionary for non-contiguous ranges
+        return inputChar.charAt(0); // fallthrough: return inputText
+      }
+    }
+
+    translateStringToUnicode(inputString) {
+      var convertedText ="";
+      var transposedChar;
+
+      for (var i = 0; i < inputString.length; i++) {
+        transposedChar = this.translateCharToUnicode(inputString.charAt(i));
+        if (transposedChar.length > 1)
+          convertedText += "&#x"+transposedChar+";"; // successful transpose to unicode
+        else
+          convertedText += transposedChar; // failed transpose
+      }
+      return convertedText;
+    }
+
+    transpose(inputString) {
+      $(this.outputTextID).text("");
+      if (inputString.length == 0) {
+        $(this.outputTextID).html("&nbsp;");
+      } else {
+        $(this.outputTextID).append(this.translateStringToUnicode(inputString));;
+      }
+    }
   }
 
-  // Converts the string in var inputText to unicode string based
-  // on the format in var format
-  function transposeStringToUnicode(inputText, format) {
-    var convertedText ="";
-    var transposedChar;
-
-    for (var i = 0; i < inputText.length; i++) {
-      transposedChar = transposeCharToUnicode(inputText.charAt(i),format)
-      if (transposedChar.length > 1)
-        convertedText += "&#x"+transposedChar+";"; // successful transpose to unicode
-      else
-        convertedText += transposedChar; // failed transpose
-    }
-    return convertedText;
-  }
 
   function transpose(){
-    var convertedText;
 
-    // clear previous outputText
-    $("#italicText").text("");
-    $("#boldText").text("");
-    $("#boldItalicText").text("");
-    $("#cursiveText").text("");
+     translatorList.forEach(function(translator, index, array){
+       translator.transpose($("#inputText").val());
+     });
 
-    // Transpose italic
-    $("#italicText").append(transposeStringToUnicode($("#inputText").val(),"I"));
-    // Transpose bold
-    $("#boldText").append(transposeStringToUnicode($("#inputText").val(),"B"));
-    // Transpose bold italic
-    $("#boldItalicText").append(transposeStringToUnicode($("#inputText").val(),"X"));
-    // Transpose bold cursive
-    $("#cursiveText").append(transposeStringToUnicode($("#inputText").val(),"C"));
-
-    if ($("#inputText").val().length == 0) {
-      $("#italicText").html("&nbsp;");
-      $("#boldText").html("&nbsp;");
-      $("#boldItalicText").html("&nbsp;");
-      $("#cursiveText").html("&nbsp;");
-    }
     // grow input textarea when needed
-    $("#inputText").height($("#boldItalicText").height());
+    if ($("#inputText")[0].scrollHeight > $("#inputText")[0].clientHeight) {
+      $("#inputText").height($("#inputText")[0].scrollHeight);
+    }
+
   }
+
+  // initilize
+  function initialize (){
+    var italicUnicodeInitialization = {
+      offset : (0x1D608 - 0x47),
+      displayName : "<h1 id='italicFont'><i>Italic</i></h1><p id='italicText' class='outputText'>&nbsp;</p>",
+      outputTextID : "#italicText",
+    };
+    var boldUnicodeInitialization = {
+      offset : (0x1D5D4 - 0x47),
+      displayName : "<h1 id='boldFont'><b>Bold</b></h1><p id='boldText' class='outputText'>&nbsp;</p>",
+      outputTextID : "#boldText",
+    };
+    var boldItalicUnicodeInitialization = {
+      offset : (0x1D63C - 0x47),
+      displayName : "<h1 id='boldItalicFont'><i><b>Bold Italic</b></i></h1><p id='boldItalicText' class='outputText'>&nbsp;</p>",
+      outputTextID : "#boldItalicText",
+    };
+    var cursiveUnicodeInitialization = {
+      offset : (0x1D4D0 - 0x47),
+      displayName : "<h1 id='cursiveFont'>Cursive</h1><p id='cursiveText' class='outputText'>&nbsp;</p>",
+      outputTextID : "#cursiveText",
+    };
+
+    translatorList.push(new asciiToUnicodeTranslator(italicUnicodeInitialization));
+    translatorList.push(new asciiToUnicodeTranslator(boldUnicodeInitialization));
+    translatorList.push(new asciiToUnicodeTranslator(boldItalicUnicodeInitialization));
+    translatorList.push(new asciiToUnicodeTranslator(cursiveUnicodeInitialization));
+
+    // write HTML to create thwir respective output fields
+    translatorList.forEach(function(translator, index, array){
+      $("#outputPanel").append(translator.writeHTML());
+    });
+  }
+
+  // need to initilize before adding event handlers for output
+  initialize();
 
   // Transpose event handlers
   $("#inputText").keyup(transpose);
+
+    // Google Analytics Events
   $("#inputText").click(function(){
     gtag('event','click',{
       'event_cataegory':'translate_input',
@@ -106,13 +125,10 @@ $(document).ready(function() {
     });
   });
 
-
-  // Google Analytics Events
   $(".outputText").on("copy",function(){
     gtag('event','click',{
       'event_cataegory':'translate_output',
       'event_label':$(this).attr("id")
     });
   });
-
 });
